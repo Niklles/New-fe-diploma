b542164349934c8a4071aabccfc310d77ffccd14
Niklles <max777944@inbox.ru>
2025-10-24 18:18:44 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/New-fe-diploma',
+  base: '/new-fe-diploma',
   server: {
     open: true,
   },
